720763c62432c884a68e96469a08cb08ef7ecee3
Hadilou1998 <hadilissilame@gmail.com>
2023-09-14 14:14:45 +0200

    add file db.sql

---
+++ API/sql/db.sql
@@ -0,0 +1,31 @@
+--  ------------------------
+--  BDD React_API
+--  ------------------------
+
+DROP DATABASE React_API;
+
+CREATE DATABASE React_API;
+
+USE React_API;
+
+--  ------------------------
+--  CREATION DES TABLES
+--  ------------------------
+
+CREATE TABLE Clients (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(25) NOT NULL,
+    password VARCHAR(25) NOT NULL,
+    role ENUM('admin', 'user') NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    adresse VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Products (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price FLOAT UNSIGNED NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    id_client INT
+);
