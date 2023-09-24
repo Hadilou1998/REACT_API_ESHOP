8a9e4f54b52d274c4e61700effba14e4e517e586
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-09-24 21:06:37 +0200

    File dbConfig.js added

---
+++ API/dbConfig.js
@@ -0,0 +1,4 @@
+const host      =   process.env.DB_HOST     ||  "localhost";
+const user      =   process.env.DB_USER     ||  "root";
+const password  =   process.env.DB_PASS     ||  "hadil";
+const database  =   process.env.DB_DATABASE ||  "Base_React";
