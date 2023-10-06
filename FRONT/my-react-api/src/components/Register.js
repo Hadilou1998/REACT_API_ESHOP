d754609b69531d414bcb9b30e469cd9e9c38354c
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-06 16:35:41 +0200

    Updated Register.js

---
+++ FRONT/my-react-api/src/components/Register.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const Register = () => 
+{
+    const [username, setUsername] = useState('');
+}
