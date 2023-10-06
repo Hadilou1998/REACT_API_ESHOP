2c3fadf1c6d883fb2001f5b8d5e400f5efe5ed4e
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-06 19:36:39 +0200

    Updated Register.js

--- FRONT/my-react-api/src/components/Register.js
+++ FRONT/my-react-api/src/components/Register.js
@@ -5,4 +5,10 @@ import { useHistory } from 'react-router-dom';
 const Register = () => 
 {
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [role, setRole] = useState('');
+    const [email, setEmail] = useState('');
+    const [adresse, setAdresse] = useState('');
+    const [codepostal, setCodepostal] = useState('');
+    const [ville, setVille] = useState('');
 }
