867b9a1353a261b5d6fc4c2d07e3dad827a47cdf
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-08 23:47:17 +0200

    Updated App.js and deleted folder components

--- FRONT/my-react-api/src/App.js
+++ FRONT/my-react-api/src/App.js
@@ -1,34 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() 
+function App()
 {
+  const [usernameReg, setUsernameReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+  const [roleReg, setRoleReg] = useState('');
+  const [emailReg, setEmailReg] = useState('');
+  const [adresseReg, setAdresseReg] = useState('');
+  const [codepostalReg, setCodepostalReg] = useState('');
+  const [villeReg, setVilleReg] = useState('');
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const register = () =>
+  {
+    Axios.post("http://localhost:8080/register",
+    {
+      username: usernameReg,
+      password: passwordReg,
+      role: roleReg,
+      email: emailReg,
+      adresse: adresseReg,
+      codepostal: codepostalReg,
+      ville: villeReg,
+    }).then((response) => 
+    {
+      console.log(response);
+    });
+  };
+
+  const login = () =>
+  {
+    Axios.post("http://localhost:8080/login",
+    {
+      username: username,
+      password: password,
+    }).then((response) =>
+    {
+      if (!response.data.message) 
+      {
+        setLoginStatus(response.data.message); 
+      }
+      else 
+      {
+        setLoginStatus(response.data[0].message);
+      }
+    });
+  };
+
   return (
     <div className="App">
       <div className="registration">
         <h1>Registration</h1>
         <label>Username</label>
-        <input type="text" /><br/>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br/>
         <label>Password</label>
-        <input type="password" /><br/>
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br/>
         <label>Role</label>
-        <input type="text" /><br/>
+        <input type="text" placeholder="Role" value={role} onChange={(e) => setRole(e.target.value)} /><br/>
         <label>Email</label>
-        <input type="text" /><br/>
+        <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /><br/>
         <label>Adresse</label>
-        <input type="text" /><br/>
+        <input type="text" placeholder="Adresse" value={adresse} onChange={(e) => setAdresse(e.target.value)} /><br/>
         <label>Codepostal</label>
-        <input type="text" /><br/>
+        <input type="text" placeholder="Codepostal" value={codepostal} onChange={(e) => setCodepostal(e.target.value)} /><br/>
         <label>Ville</label>
-        <input type="text" /> <br />
-        <button>Register</button>
+        <input type="text" placeholder="Ville" value={ville} onChange={(e) => setVille(e.target.value)} /> <br />
+        <button onClick={register}>Register</button>
       </div>
       <div className="login">
         <h1>Login</h1>
-        <input type="text" placeholder="Username" /> <br/>
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /> <br/>
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={login}>Login</button>
       </div>
+      <h1>{loginStatus}</h1>
     </div>
   );
 }
