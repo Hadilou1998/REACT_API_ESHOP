ee54313ee9b9e406a19532da12b29a6f00573979
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-08 22:53:02 +0200

    Updated App.css, App.js and index.js in src folder

--- FRONT/my-react-api/src/App.js
+++ FRONT/my-react-api/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input type="text" /><br/>
+        <label>Password</label>
+        <input type="password" /><br/>
+        <label>Role</label>
+        <input type="text" /><br/>
+        <label>Email</label>
+        <input type="text" /><br/>
+        <label>Adresse</label>
+        <input type="text" /><br/>
+        <label>Codepostal</label>
+        <input type="text" /><br/>
+        <label>Ville</label>
+        <input type="text" /> <br />
+        <button>Register</button>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" /> <br/>
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
     </div>
   );
 }
