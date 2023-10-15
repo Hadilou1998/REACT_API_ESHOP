6416c22047e6e3ca53411aad248ad05a506446d7
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-15 17:32:07 +0200

    Updated App.jsx

--- FRONT/my-react-api/src/App.jsx
+++ FRONT/my-react-api/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import { Register, Login } from './components';
+import Register from './components/Register';
+import Login from './components/Login';
 import Home from './components/Home';
 
 function App()
