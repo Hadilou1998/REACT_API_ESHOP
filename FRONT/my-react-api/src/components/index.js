ee417500eefe19a7531453e4360f725c436bacca
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2023-10-28 18:06:42 +0200

    Updated all files in components and src

--- FRONT/my-react-api/src/components/index.js
+++ FRONT/my-react-api/src/components/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
+
 
 axios.defaults.withCredentials = true;
 
