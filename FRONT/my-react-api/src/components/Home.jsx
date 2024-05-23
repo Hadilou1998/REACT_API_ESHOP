1e72581c534532f44e0e733a3137947d3ecd2602
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-05-23 09:28:33 +0200

    Updated Home.jsx

--- FRONT/my-react-api/src/components/Home.jsx
+++ FRONT/my-react-api/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-import { useHistory } from "react-router-dom";
+/*import { useHistory } from "react-router-dom";*/
 
 const Home = () =>
 {
@@ -9,7 +9,7 @@ const Home = () =>
     const [token, setToken] = useState('');
     const [expire, setExpire] = useState('');
     const [users, setUsers] = useState([]);
-    const history = useHistory();
+    /*const history = useHistory();*/
 
     useEffect(() => 
     {
@@ -36,6 +36,32 @@ const Home = () =>
     }
 
     const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            const response = await axios.get("http://localhost:8080/token");
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            setToken(response.data.accessToken);
+            const decoded = jwtDecode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);       
+        }
+        return config;
+    },
+    (error) =>
+    {
+        return Promise.reject(error);
+    });
+
+    const getUsers = async () = 
+    {
+        const response = await axiosJWT.get("http://localhost:8080/users", {
+            headers: {
+
+            }
+        })
+    }
 };
 
 export default Home;
