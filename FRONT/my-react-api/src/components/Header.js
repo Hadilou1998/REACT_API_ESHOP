7f1fe35aeddfe21b24b4e586d2ce8574bd41612f
Hadilou1998 <hadilissilame@gmail.com>
2024-10-11 12:47:54 +0200

    Updated Project

--- FRONT/my-react-api/src/components/Header.js
+++ FRONT/my-react-api/src/components/Header.js
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
-import {Link} from "react-router-dom";
-import AuthOptions from "./AuthOptions"; 
+import React from "react";
+import {Link} from "react-router-dom"; 
 
-class Header extends Component 
-{
-    render() 
-    {
-        return (
-            <header className="header">
-                <Link to="/"><h1 className="title">Welcome to my App MERN</h1></Link>
-                <AuthOptions />
-            </header>
-        );
-    }
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
+                    <li><Link to="/checkout">Checkout</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li>{localStorage.getItem('token')? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>}</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default Header;
